49701e89c20290b710eee88b76e1687d35023d92
Ismo236 <ismaeltchagnani@gmail.com>
2026-03-10 11:11:38 +0100

    modif 1 back

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,9 +11,9 @@ use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
-    public function login(Request $request)
-    {
+
+    public function login(Request $request){
+
         try{
             $validator = Validator::make($request->all(), [
                 'email' => 'email|required',
@@ -22,29 +22,30 @@ class AuthController extends Controller
 
             if ($validator->fails()) {
                 return response()->json([
-                    "status"=> "error",    'message'=> "Paramètres incorrects" ], 401);  //
+                    "status"=> "error",  'message'=> "Paramètres incorrects" ], 401);  //
             }
 
-
-
             $loginData = $request->only('email', 'password');
+            
             if (!auth()->attempt($loginData)) {
-                return response([ "status"=> "erreur",    'message' => 'Paramètres invalides']);
+                return response([ "status"=> "erreur", 'message' => 'Paramètres invalides']);
             }
             // si le user est actif
             if(auth()->user() != null ) {
                 $accessToken = auth()->user()->createToken('authToken')->accessToken;
-                return response(['user' => auth()->user(), 'access_token' => $accessToken, "status"=> "success",  ]);
+                return response(['user' => auth()->user(), 'access_token' => $accessToken, "status"=> "success",]);
             } 
 
         }catch(Exception $ex){
             Log::error($ex->getMessage());
             return response()->json([
                 'error'=>"error",
-                'message'=>"Une erreur interne est survenue.",
+                'message'=>"Une erreur interne est survenue OKKK.",
                 'statut'=>500
             ],500);
         }
 
-    }//end login    
+    }//end login 
+
+    
 }
